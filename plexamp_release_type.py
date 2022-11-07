f8ae2bf75764c7ac56bda92f6dbb41ee08a2de97
trevordavies095 <trevordavies095@gmail.com>
2022-11-07 09:42:09 -0500

    plexamp rewrite

--- plexamp_release_type.py
+++ plexamp_release_type.py
@@ -1,8 +1,6 @@
 from mutagen.flac import FLAC
+import argparse
 import os
-
-directory = "/Volumes/Backup/Music/"
-filetypes = (".flac")
 
 plex_release_types = {
     "live": "album;live",
@@ -12,36 +10,40 @@ plex_release_types = {
     "soundtrack": "album;soundtrack"
 }
 
-for root, dirs, files in os.walk(directory):
-    for file in files:
-        if file.endswith(filetypes):
-            if file.endswith(".flac"):
-                try:
-                    audio = FLAC(root + "/" + file)
-                    rt = audio["releasetype"][0].lower().strip()
-                except:
-                    continue
 
-                if rt == "live":
-                    audio["releasetype"] = plex_release_types["live"]
-                    print(root + "/" + file + " - UPDATED TO " + plex_release_types["live"])
-                    audio.save()
-                elif rt == "compilation":
-                    audio["releasetype"] = plex_release_types["compilation"]
-                    print(root + "/" + file + " - UPDATED TO " + plex_release_types["compilation"])
-                    audio.save()
+def main():
+    args = term_args()
+        
+    if args.path is None:
+        print("Please provide directory!")
+        exit(1)
 
-                elif rt == "remix":
-                    audio["releasetype"] = plex_release_types["remix"]
-                    print(root + "/" + file + " - UPDATED TO " + plex_release_types["remix"])
-                    audio.save()
+    directory = args.path
+    filetypes = (".flac")
 
-                elif rt == "mixtape":
-                    audio["releasetype"] = plex_release_types["mixtape"]
-                    print(root + "/" + file + " - UPDATED TO " + plex_release_types["mixtape"])
-                    audio.save()
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith(filetypes):
+                if file.endswith(".flac"):
+                    try:
+                        audio = FLAC(root + "/" + file)
+                        rt = audio["releasetype"][0].lower().strip()
+                        p = root + "/" + file
+                        update_release_type(audio, rt, p)
+                    except:
+                        continue
 
-                elif rt == "soundtrack":
-                    audio["releasetype"] = plex_release_types["soundtrack"]
-                    print(root + "/" + file + " - UPDATED TO " + plex_release_types["soundtrack"])
-                    audio.save()
+def update_release_type(track, rt, p):
+    track["releasetype"] = plex_release_types[rt]
+    print(p + " - UPDATED TO " + plex_release_types[rt])
+    track.save()
+
+
+def term_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--path", type=str, help="Path to library.")
+    return parser.parse_args()
+
+
+if __name__ == '__main__': 
+    main()
