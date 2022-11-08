d8d96590f9ef5fcf37fbefcbe88d9cab16cdfd16
Benjamen Alford <benjamen.alford@gmail.com>
2022-11-08 11:25:08 -0600

    tested creation

--- recursive_transcode.py
+++ recursive_transcode.py
@@ -4,21 +4,22 @@ import argparse
 import ffmpeg
 
 lossless_extensions = (".flac", ".wav")
+destination_codec = 'alac'
 
 def main():
 	args = term_args()
 	
 	if args.path is None or args.output_path is None:
-		print('input path or output path not specified')
-		exit(1)
-
-	print(f'Processing {args.path} ')
-	
+        	print('input path or output path not specified')
+        	exit(1)
+		
 	base_dir = args.path
 	output_dir =  args.output_path
 	print(f'Base Directory  = { base_dir}')
 	print(f'Output Directory = {output_dir}')
-	
+
+	print(f'Output Codec = {destination_codec}')
+
 	for root, subs, files in os.walk(base_dir):
 		for file in files:
 			if file.endswith(lossless_extensions):	
@@ -26,10 +27,16 @@ def main():
 
 def convert(file, path, output_dir):
 	print(file)
+	file_folder = os.path.basename(path)
+	destination_folder =  os.path.join(output_dir, file_folder)
+	if (os.path.exists(destination_folder) is False):
+		os.mkdir(destination_folder)
+
 	for file_extension in lossless_extensions:
 		if (file.endswith(file_extension)):
 			out_file = file.split(file_extension)[0] 
-	cmd = f'ffmpeg -i "{os.path.join(path , file)}" -y -v 0 -vcodec copy -acodec alac "{output_dir}/{out_file}.m4a"'
+	cmd = f'ffmpeg -i "{os.path.join(path , file)}" -y -v 0 -vcodec copy -acodec {destination_codec} "{os.path.join(destination_folder,out_file)}.m4a"'
+	
 	os.system(cmd)
 	
 def term_args():
